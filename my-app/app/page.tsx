de2d210eb9632ff5e148a621257ddd01b2b2a492
chil-t <jayar@DESKTOP-QVJNGP5>
2024-07-19 00:12:38 -0700

    Install Next.js

---
+++ my-app/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    // specialization (Light, Medium, Heavy) 
+    // Light
+        // Cloacking Device -> 1
+        // Evasive Dash -> 2
+        // Grappling Hook -> 3
+    // generate a random number between 1 and 3
+    //
+    return <h1>test</h1>;
+}
